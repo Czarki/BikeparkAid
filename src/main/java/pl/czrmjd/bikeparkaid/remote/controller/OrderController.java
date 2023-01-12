73493162cf5dc99f732cc294a599382a8402c4d4
Cezary Majda <czrmjd45@gmail.com>
2023-01-12 20:34:05 +0100

    log order

--- src/main/java/pl/czrmjd/bikeparkaid/remote/controller/OrderController.java
+++ src/main/java/pl/czrmjd/bikeparkaid/remote/controller/OrderController.java
@@ -3,6 +3,7 @@ package pl.czrmjd.bikeparkaid.remote.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.ls.LSOutput;
 import pl.czrmjd.bikeparkaid.remote.model.OrderDto;
 import pl.czrmjd.bikeparkaid.service.AccessTokenService;
 import pl.czrmjd.bikeparkaid.service.OrderService;
@@ -30,6 +31,13 @@ public class OrderController {
             return ResponseEntity.status(HttpStatus.FORBIDDEN)
                 .build();
         }
+        System.out.println(accessToken);
+        System.out.println("pass" + orderDto.getPassId());
+        System.out.println("bike" + orderDto.getBikeId());
+        System.out.println("DateStart" + orderDto.getBikeDateStart());
+        System.out.println("DateEnd" + orderDto.getBikeDateEnd());
+        System.out.println("training" + orderDto.getTrainingId());
+        System.out.println(userId);
         return orderService.create(orderDto.getPassId(),
             orderDto.getBikeId(),
             convertToDate(orderDto.getBikeDateStart()),
